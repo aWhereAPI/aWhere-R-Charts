383c6a97cf3e6eae5979235ced9aa6fce98eb389
Drew Marticorena <drewmarticorena@awhere.com>
2020-09-11 10:30:05 -0400

    made it so that the scaling fxn when plotting multiple axis works much more effectlivly

--- R/generateaWhereChart.R
+++ R/generateaWhereChart.R
@@ -53,6 +53,7 @@
 #' @import ggthemes
 #' @import zoo
 #' @import data.table
+#' @import scales
 #'
 #' @return plot object
 #'
@@ -89,9 +90,8 @@ generateaWhereChart <- function(data
   ylabel          <- list()
   chart_data      <- list()
   chart_data_long <- list()
-  scalingFactor   <- list()
-  offsetFactor    <- list()
   colorScheme     <- list()
+  rangeToUse      <- list()
   
   # variables for title and label font sizes 
   size_font_main_title <- 16
@@ -423,6 +423,7 @@ generateaWhereChart <- function(data
                                   ,by = 'date')
     
     setkey(chart_data_long[[x]],Variable,date)
+    
   }
   
   #if title is not given by user, set it to date range + variable
@@ -445,14 +446,19 @@ generateaWhereChart <- function(data
   for (x in 1:length(ylabel_unique)) {
     currentIndices <- which(ylabel == ylabel_unique[x])
     
-    if (x == 1) {
-      rangeToUse <- diff(rbindlist(chart_data_long[currentIndices])[,quantile(x = measure,na.rm = TRUE,probs = c(0,1))])
-      offsetFactor[currentIndices] <- 0
-      scalingFactor[currentIndices] <- 1
-    } else {
-      #offsetFactor[[x]] <- chart_data_long[[x]][,min(measure)]
-      #scalingFactor[[x]] <- (diff(chart_data_long[[x]][,quantile(x = measure,na.rm = TRUE,probs = c(0,1))])/2)/rangeToUse
-      scalingFactor[currentIndices] <- rbindlist(chart_data_long[currentIndices])[,quantile(x = measure,na.rm = TRUE,probs = c(1))]/rangeToUse
+    rangeToUse[[currentIndices]] <- rbindlist(chart_data_long[currentIndices])[,quantile(x = measure,na.rm = TRUE,probs = c(0,1))]
+    
+    if (x > 1) {
+
+      for (z in 1:length(currentIndices)) {
+       
+         chart_data_long[[currentIndices[z]]][,measure := scales::rescale(x = measure
+                                                                        ,from = c(min(measure
+                                                                                      ,na.rm = TRUE)
+                                                                                  ,max(measure
+                                                                                       ,na.rm = TRUE))
+                                                                        ,to = rangeToUse[[1]])]
+      }
     }
   }
   
@@ -613,14 +619,19 @@ generateaWhereChart <- function(data
         chart +
         geom_ribbon(data = rbindlist(chart_data_long[currentIndices])
                   ,aes(x = date
-                       ,ymin = measure/unique(unlist(scalingFactor[currentIndices]))
-                       ,ymax = measure/unique(unlist(scalingFactor[currentIndices]))
+                       ,ymin = measure
+                       ,ymax = measure
                        ,colour = Variable
                        ,fill = Variable)
                   ,size = line_width
                   #,linetype = linetypes[x] # change line type for sunsequent variables
                   ) +
-        scale_y_continuous(sec.axis = sec_axis(~.*unique(unlist(scalingFactor[currentIndices]))
+        #we need to map the values back to the original scale for plotting on
+        #the side if Multiple values were to exist for currentIndices they will
+        #have the same info stored, hence we can take the first
+        scale_y_continuous(sec.axis = sec_axis(~ scales::rescale(x = .
+                                                                    ,from= rangeToUse[[1]]
+                                                                    ,to = rangeToUse[currentIndices][[1]])
                                                ,name = unique(unlist(ylabel[currentIndices])))) 
       
       numFillsLegend <- numFillsLegend + rbindlist(chart_data_long[currentIndices])[,length(unique(Variable))]
